ac9f6a8dcb696814d5e626772e97ad4093c47e36
Anus Ali Khan <anusalikhan1405@gmail.com>
2023-07-31 23:45:53 +0500

    working on redux project

---
+++ src/components/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Profile() {
+    return <div>
+        <h1>Profile page</h1>
+        <p>Name:</p>
+        <p>Age:</p>
+        <p>Email:</p>
+    </div>
+}
+
+export default Profile;

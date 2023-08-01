3734528bca330bbd215980d73ea478874de973eb
Anus Ali Khan <anusalikhan1405@gmail.com>
2023-08-01 23:39:23 +0500

    logout functionality  added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { login, logout } from '../features/users';
 
 function Login() {
+    const dispatch = useDispatch();
+
     return <div>
-        <button>Login</button>
+        <button
+            onClick={() => {
+                dispatch(login({ name: "Pedro", age: 20, email: "Pedro@gmail.com" }))
+            }}>
+            Login
+        </button>
+        <button onClick={() => {
+            dispatch(logout())
+        }}>
+            LOGOUT
+        </button>
     </div>
 }
 
